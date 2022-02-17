210f0a8b73ae60121877b3ece0ecf340e0eb9136
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-02-17 12:23:25 +0600

    last update

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-// ===========All input Number retune function====================
-function inputValue(inputId) {
-  const inputField = document.getElementById(inputId + "-input");
-  const inputValue = inputField.value;
-  //===============Check input Value =================
-  if (isNaN(inputValue)) {
-    cleck("toast", inputId, "valid");
-  } else if (inputValue < 0) {
-    cleck("toast", inputId, "Positive");
-  }
-  const inputNumber = parseFloat(inputValue);
-
-  return inputNumber;
-}
-
+//Button Event Handle
 document.getElementById("calculate-btn").addEventListener("click", function () {
   calculate();
 });
 document.getElementById("save-btn").addEventListener("click", function () {
   saveMoneyCalculate();
 });
+
+// ===========All input Number retune function====================
+function inputValue(inputId) {
+  const inputField = document.getElementById(inputId + "-input");
+  const inputValue = inputField.value;
+
+  //===============Check input Value =================
+  if (isNaN(inputValue)) {
+    cleck("toast", inputId, "valid");
+  } else if (inputValue < 0) {
+    cleck("toast", inputId, "Positive");
+  } else {
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+  }
+}
+
 //==================Check Function===============
 function cleck(toastId, inputId, check) {
   const toast = document.getElementById(toastId);
@@ -37,7 +40,9 @@ function calculate() {
 
   const totalExpenses = foodNumber + RentNumber + ClothesNumber;
   const totalBalance = income - totalExpenses;
-
+  if (totalExpenses > totalBalance) {
+    cleck("toast", "TotalExpenses amount more than your Balance account", "");
+  }
   document.getElementById("total-expenses").innerText = totalExpenses;
   document.getElementById("total-balance").innerText = totalBalance;
 }
@@ -51,6 +56,9 @@ function saveMoneyCalculate() {
 
   const saveAmount = (income * saveMoneyParcent) / 100;
   const RemainingBalance = parseFloat(totalBalance) - saveAmount;
+  if (saveAmount > totalBalance) {
+    cleck("toast", "amount more than your savings account balance", "");
+  }
   //====text Vavlue
   document.getElementById("save-amount").innerText = saveAmount;
   document.getElementById("remaining-balance").innerText = RemainingBalance;
